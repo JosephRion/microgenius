63c9b7765dd044c288084227a88b04e555a0a069
taro.kirameki <femyoryu221@mirai.re>
2022-08-13 10:59:13 +0000

    microgenius project started. 2022.08.13..1359TKT

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \URL::forceScheme('https'); //Lesson 15Chapter 3.1 プロジェクトの作成 2022.08.13
+    }
+}

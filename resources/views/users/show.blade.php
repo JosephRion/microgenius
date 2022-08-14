a60e40633fef8616960a5e950966094a0acea5fa
taro.kirameki <femyoryu221@mirai.re>
2022-08-14 11:21:57 +0000

    L15 C10.5 follow, unfollow C10 completed. 2022.08.14..1420TKT

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,36 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+{{--L15 C8.4 UsersControllerなどあったが、L15 C10.4 show で大幅に書き換え--}}
     <div class="row">
         <aside class="col-sm-4">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">{{ $user->name }}</h3>
-                </div>
-                <div class="card-body">
-                    {{-- ユーザのメールアドレスをもとにGravatarを取得して表示. アバターの表示部分にはBootstrapのカードを利用。L15 C8.4 UsersController --}}
-                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
-                </div>
-            </div>
-            
-            {{-- フォロー／アンフォローボタン --}}
-            @include('user_follow.follow_button')
-            
+            {{-- ユーザ情報 --}}
+            @include('users.card')
         </aside>
         <div class="col-sm-8">
-            <ul class="nav nav-tabs nav-justified mb-3">
-                {{-- ユーザ詳細タブ --}}
-                <li class="nav-item">
-                    <a href="{{ route('users.show', ['user' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}">
-                        TimeLine
-                        <span class="badge badge-secondary">{{ $user->microgeniuses_count }}</span>
-                    </a>
-                </li>
-                {{-- フォロー一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
-                {{-- フォロワー一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
-            </ul>
+            {{-- タブ --}}
+            @include('users.navtabs')
             @if (Auth::id() == $user->id)
                 {{-- 投稿フォーム --}}
                 @include('microgeniuses.form')

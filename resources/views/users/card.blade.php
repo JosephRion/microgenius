7b81396ee0c1ade6c00ef84f9cc44dd084adc4fe
taro.kirameki <femyoryu221@mirai.re>
2022-08-17 20:47:37 +0000

    under the profile photo, messages fixed. all posts can be edited after posting. Everything works. 2022.08.17..2347TKT. Well-done!

--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -5,25 +5,19 @@
     <div class="card-body">
         {{-- L15 C10.4 view でファイル新規作成ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
         <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
-        この部位は全員表示される。
-        Auth::id() = {!! Auth::id()  !!},
-        $ user-> ={!! $user->id !!} である。
         
-                        {{-- Viewに削除ボタンをつけ足します。 --}}
-                        <div>
-                            {{--ログインユーザーの時のみこれが表示されるようにする。--}}
-                            @if (Auth::id() == $user->id)
-                                ログインユーザーのときだけこれが表示される。ifの中でも
-                                Auth::id() = {!! Auth::id()  !!},
-                                $ user-> ={!! $user->id !!} である。
-                            {{-- プロフィール編集ページへのリンク --}}
-                            {{-- {!! link_to_route('messages.edit', 'プロフィールを編集', ['message' => $message->id], ['class' => 'btn btn-light']) !!}
-                            {!! link_to_route('users.edit', 'プロフィールを編集', ['user' =>Auth::id()], ['class' => 'btn btn-light']) !!}--}}
-                            {{-- プロフィール編集ページへのリンク--}}
-
-                            @endif
-                        </div>
-
+         {{-- Viewに削除ボタンをつけ足します。 
+            Auth::id() = {!! Auth::id()  !!},
+            $ user-> ={!! $user->id !!} である。
+                <div>
+                    ログインユーザーの時のみこれが表示されるようにする。
+                    @if (Auth::id() == $user->id)
+                        ログインユーザーのときだけこれが表示される。ifの中でも
+                        Auth::id() = {!! Auth::id()  !!},
+                        $ user-> ={!! $user->id !!} である。
+                    @endif
+                </div>
+        --}}
     </div>
 </div>
 {{-- フォロー／アンフォローボタン --}}

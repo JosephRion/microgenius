6be86dfae5f44bd4bfc5966a2247a2fd6ee2c1fa
taro.kirameki <femyoryu221@mirai.re>
2022-08-17 20:28:16 +0000

    Mictogeniuses Post Edit and Update all competed. 2022.08.17..2328TKT

--- resources/views/microgeniuses/edit.blade.php
+++ resources/views/microgeniuses/edit.blade.php
@@ -1,39 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-{{--cf)    ここにページ毎のコンテンツを書く 
+{{--
+参考ファイルは
 message-board/resources/views/messages/edit.blade.php
 
 このファイルは
 microgenius/resources/views/microgeniuses/edit.blade.php
-エラー表示確認用
-@if (count($errors) > 0)
-        <ul class="alert alert-danger" role="alert">
-            @foreach ($errors->all() as $error)
-                <li class="ml-4">{{ $error }}</li>
-            @endforeach
-        </ul>
-@endif
---}}   
+--}}
 
-
-
-    <h1>id: {{ $microgeniuse->id }} のメッセージ編集ページ。</h1>
+    <h1>id: {{ $microgeniuse->id }} のメッセージ編集ページ</h1>
 
     <div class="row">
         <div class="col-6">
-            {{-- 
             {!! Form::model($microgeniuse, ['route' => ['microgeniuses.update', $microgeniuse->id], 'method' => 'put']) !!}
-
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', '投稿内容:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
-
-                {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
-
+                
+                {!! Form::submit('Update!', ['class' => 'btn btn-primary']) !!}
             {!! Form::close() !!}
         </div>
     </div>
---}}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get'); //| Met
 
 //L15 C8.2//Router認証付きのルーティング //これで、認証済みのユーザだけがこれらのアクションにアクセスできます。
 Route::group(['middleware' => ['auth']], function () {
+    //Users
     //L15 C10.2 Router 追加
     Route::group(['prefix' => 'users/{id}'], function () { //認証付きの中で、followとお気に入り系。2022.08.16で投稿編集のルーティングを追加 //prefixを使用すると、第一引数に毎回 users と指定しなくても良くなります。あくまでも 重複する記述をまとめて簡潔にする だけの機能になります。
         Route::post('follow', 'UserFollowController@store')->name('user.follow'); //| Method=POST | URI=users/{id}/follow | Name=user.follow | Action=App\Http\Controllers\UserFollowController@store | web,auth |
@@ -43,7 +44,10 @@ Route::group(['middleware' => ['auth']], function () {
     //L15 C8.2
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]); //| GET|HEAD | users                         | users.index           | App\Http\Controllers\UsersController@index                        | web,auth     |
                                                                                 //| GET|HEAD | users/{user}                  | users.show            | App\Http\Controllers\UsersController@show                         | web,auth     |
-
+    //Route::get('profile/{id}/edit', 'ProfileController@edit')->name('profile.edit'); //プロフィールの編集             
+    
+    ////---------------------------------------------------------------//---------------------------------------------------------------//---------------------------------------------------------------
+    //microgeniuses
     // 追加L15 C13.1 認証付きの中で、お気に入り用に追加 ここから 2022.07.13..1415TKT
     Route::group(['prefix' => 'microgeniuses/{id}'], function () { //
         Route::post('favorite', 'FavoritesController@store')->name('favorites.favorite'); //        | POST     | microgeniuses/{id}/favorite   | favorites.favorite    | App\Http\Controllers\FavoritesController@store                    | web,auth     |
@@ -54,12 +58,12 @@ Route::group(['middleware' => ['auth']], function () {
     
     //L15C9.2 Router
     //認証を必要とするルーティンググループ内に、 Microgeniusesのルーティングを設定します（登録のstoreと削除のdestroyのみ）。
-    Route::resource('microgeniuses', 'MicrogeniusesController', ['only' => ['store', 'destroy']]);  //| POST     | microgeniuses                 | microgeniuses.store   | App\Http\Controllers\MicrogeniusesController@store                | web,auth     |
-    
-    //投稿の編集ページのURLへのアクセスが有った時の処理。
-    //Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit'); //投稿の編集。まずは投稿の編集から。2022.08.17..1135TKT
+    //Route::resource('microgeniuses', 'MicrogeniusesController', ['only' => ['store', 'destroy']]);  //| POST     | microgeniuses                 | microgeniuses.store   | App\Http\Controllers\MicrogeniusesController@store                | web,auth     |
+    //store, destroyに updateを追加。
+    Route::resource('microgeniuses', 'MicrogeniusesController', ['only' => ['store', 'destroy', 'update' ]]);  //|        | PUT|PATCH | microgeniuses/{microgenius}   | microgeniuses.update  | App\Http\Controllers\MicrogeniusesController@update               | web,auth     |
+    //投稿の編集ページのURLへのアクセスが有った時の処理。//投稿の編集。まずは投稿の編集から。2022.08.17..1135TKT
     Route::get('microgeniuses/{id}/edit', 'MicrogeniusesController@edit')->name('microgeniuses.edit'); //投稿の編集。ルーティングを通すために、まずこっちでやってみる。2022.08.17..1153TKT
-    //Route::get('profile/{id}/edit', 'ProfileController@edit')->name('profile.edit'); //プロフィールの編集                                                                                             //| DELETE   | microgeniuses/{microgenius}   | microgeniuses.destroy | App\Http\Controllers\MicrogeniusesController@destroy              | web,auth     |
+                                                                                    //| DELETE   | microgeniuses/{microgenius}   | microgeniuses.destroy | App\Http\Controllers\MicrogeniusesController@destroy              | web,auth     |
 }); //Router認証付きのルーティングの閉じ括弧
 
 //ユーザが自分の名前を編集するアクション（edit, update)や退会アクション（destroy)を作っても問題ありませんし、
